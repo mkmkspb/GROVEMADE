08433b7391e18fc4dd5fe518facc086333a66ec7
Konstantin Muzyka <kosatkinms@mail.ru>
2021-11-03 01:33:29 +0300

    Добавлены img, css, js

---
+++ js/main.js
@@ -0,0 +1,11 @@
+const goUpButton = document.getElementById("goup");
+
+window.addEventListener("scroll", function () {
+	"use strict";
+
+	if (scrollY > 1200) {
+		goup.style.display = "block";
+	} else {
+		goup.style.display = "none";
+	}
+});
